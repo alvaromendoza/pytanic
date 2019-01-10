a83bcd34a9cee61dedc61b3e68363be1e83bed26
Alvaro Mendoza <skjoeldr@gmail.com>
2019-01-10 14:20:06 +0200

    Fix logging and kwargs in CrossValidatedClassifier

--- src/titanic/modelling.py
+++ src/titanic/modelling.py
@@ -1,5 +1,6 @@
 """Modelling utility functions and classes"""
 
+import sys
 import time
 import datetime
 import pickle
@@ -61,11 +62,12 @@ class DataFrameDummifier(BaseEstimator, TransformerMixin):
 class CrossValidatedClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self, clf):
         self.clf = clf
-        self.cvs_history = dict()
+        self.cvs_stamp = dict()
         self.init_params = clf.get_params()
         self.best_params = None
         self._params_strategy = 'init'
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(__name__ + '_' + self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
@@ -75,40 +77,39 @@ class CrossValidatedClassifier(BaseEstimator, ClassifierMixin):
         predicted = self.clf.predict(X)
         return predicted
 
-    def _print_cvs_history(self, depth=3):
-        key = max(self.cvs_history)
-        for r in range(depth):
-            if key == 0:
-                break
-            print(f"RUN {key} score: {self.cvs_history[key]['score']}")
-            key = key - 1
-
-    def cross_val_score(self, X, y, scoring='accuracy', hprint=True, hprint_depth=3, **kwargs):
+    def cross_val_score(self, X, y, scoring='accuracy', print_cvs=True, **kwargs):
         start_time = time.time()
         score = cross_val_score(self.clf, X, y, scoring=scoring, **kwargs).mean()
         end_time = time.time()
-        if self.cvs_history == dict():
-            key = 1
-        else:
-            key = max(self.cvs_history) + 1
-        self.cvs_history[key] = dict()
-        self.cvs_history[key]['timestamp'] = (datetime.datetime
-                                              .fromtimestamp(start_time)
-                                              .strftime('%Y-%m-%d %H:%M:%S'))
-        self.cvs_history[key]['executed_in'] = end_time - start_time
-        self.cvs_history[key]['score'] = score
-        if hprint:
-            self._print_cvs_history(depth=hprint_depth)
+        self.cvs_stamp['timestamp'] = (datetime.datetime
+                                       .fromtimestamp(start_time)
+                                       .strftime('%Y-%m-%d %H:%M:%S'))
+        self.cvs_stamp['executed_in'] = end_time - start_time
+        self.cvs_stamp['scoring'] = scoring
+        self.cvs_stamp['score'] = score
+        self.cvs_stamp['params_strategy'] = self._params_strategy
+        self.cvs_stamp['clf_params'] = self.clf.get_params()
+        if print_cvs:
+            print('\n-------------CROSS-VALIDATION-------------\n')
+            print(scoring, ': ', self.cvs_stamp['score'], sep='')
         return score
 
-    def grid_search_cv(self, X, y, param_grid, **kwargs):
-        grid = GridSearchCV(self.clf, param_grid=param_grid, **kwargs)
+    def grid_search_cv(self, X, y, param_grid, scoring='accuracy', print_gscv=True, **kwargs):
+        grid = GridSearchCV(self.clf, param_grid, scoring=scoring, **kwargs)
         grid.fit(X, y)
         self.best_params = grid.best_params_
-        print('best_params:')
-        print(grid.best_params_)
-        print('best_score:')
-        print(grid.best_score_)
+        if print_gscv:
+            logger = logging.getLogger(__name__ + '_' +
+                                       self.__class__.__name__ +
+                                       '_grid_search_cv')
+            logger.setLevel(logging.INFO)
+            handler = logging.StreamHandler(sys.stdout)
+            logger.addHandler(handler)
+            logger.info('---------------GRID SEARCH----------------')
+            logger.info('\nbest_params:\n' + str(grid.best_params_))
+            logger.info('\nbest_score: ' + str(grid.best_score_))
+            handler.close()
+            logger.removeHandler(handler)
         return grid.best_params_, grid.best_score_
 
     @property
@@ -130,24 +131,40 @@ class CrossValidatedClassifier(BaseEstimator, ClassifierMixin):
         tools.serialize(self, file_path=file_path)
 
     @classmethod
-    def train(cls, clf, X, y, param_grid=None, param_strategy='init', logdir_name=None, serialize_to=None):
+    def train(cls, clf, X, y, param_grid=None, param_strategy='init',
+              print_cvs=True, print_gscv=True,
+              logdir_name=None, serialize_to=None, sklearn_gscv_kws=None,
+              sklearn_cvs_kws=None):
+
+        # Counter mutable default kwargs
+        if sklearn_gscv_kws is None:
+            sklearn_gscv_kws = dict()
+        if sklearn_cvs_kws is None:
+            sklearn_cvs_kws = dict()
+
+        # Train model
         model = cls(clf)
         if param_grid is not None:
-            model.grid_search_cv(X, y, param_grid)
+            model.grid_search_cv(X, y, param_grid, print_gscv=print_gscv, **sklearn_gscv_kws)
             model.params_strategy = param_strategy
-        model.cross_val_score(X, y)
+        model.cross_val_score(X, y, print_cvs=print_cvs, **sklearn_cvs_kws)
+
+        # Serialize model
         if serialize_to is not None:
             model.serialize(serialize_to)
+
+        # Write logs
         if logdir_name is not None:
+            logger = logging.getLogger(__name__ + '_' + cls.__name__ + '_train')
             logdir_path = PROJECT_ROOT_ABS_PATH / 'logs' / 'models' / logdir_name
             logdir_path.mkdir(parents=True, exist_ok=True)
-            logfile_name = (model.cvs_history[1]['timestamp'] + r'.log').replace(r':', r'-')
+            logfile_name = (model.cvs_stamp['timestamp'] + r'.log').replace(r':', r'-')
             file_handler = logging.FileHandler(logdir_path / logfile_name, mode='w')
-            model.logger.addHandler(file_handler)
-            model.logger.setLevel(logging.DEBUG)
-            model.logger.info(model.cvs_history)
+            logger.addHandler(file_handler)
+            logger.setLevel(logging.INFO)
+            logger.info(model.cvs_stamp)
             file_handler.close()
-            model.logger.removeHandler(file_handler)
+            logger.removeHandler(file_handler)
         return model
 
 
@@ -174,7 +191,8 @@ if __name__ == '__main__':
 #    logreg.cross_val_score(X_train, y_train)
     param_grid = {'logisticregression__C': [0.8, 1, 1.2]}
 #    logreg.grid_search_cv(X_train, y_train, param_grid)
-    logreg = CrossValidatedClassifier.train(pipe, X_train, y_train, param_grid, logdir_name='logreg')
+    logreg = CrossValidatedClassifier.train(pipe, X_train, y_train, param_grid,
+                                            logdir_name='logreg')
 
 
 
