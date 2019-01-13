81b507a58730ae56fb4fe68b8bdaa6402151e05a
Alvaro Mendoza <skjoeldr@gmail.com>
2019-01-13 16:11:39 +0200

    Add clean_directory function to tools

--- src/titanic/tools.py
+++ src/titanic/tools.py
@@ -1,6 +1,22 @@
 """Technical utility functions"""
 
+import os
 import pickle
+
+
+def clean_directory(dir_path, keep_nested_dirs=False, files_to_keep=None):
+    if files_to_keep is None:
+        files_to_keep = list()
+    for root, dirnames, filenames in os.walk(dir_path, topdown=False):
+        for filename in filenames:
+            if filename not in files_to_keep:
+                os.unlink(os.path.join(root, filename))
+        if not keep_nested_dirs:
+            for dirname in dirnames:
+                try:
+                    os.rmdir(os.path.join(root, dirname))
+                except OSError:
+                    continue
 
 
 def serialize(obj, file_path=None):
