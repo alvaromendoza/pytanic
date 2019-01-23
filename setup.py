a04ebb7871b3e214da6e84d5fdfad1c57d1a1dfb
Alvaro Mendoza <skjoeldr@gmail.com>
2019-01-23 13:24:09 +0200

    Add setuptools_scm to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='titanic',
-    # packages=find_packages(),
 	packages=['titanic'],
-    # package_dir={'': 'src'},
 	package_dir={'': 'src'},
-    version='0.0.0',
+	use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     description='Demo data science project using Titanic dataset from Kaggle.',
     author='Alvaro Mendoza',
     license='MIT',
